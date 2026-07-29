7652a1f684a49ffc40e2fa6f5035850eea0919c3
Laurie Mavoungou <lm9469@rit.edu>
2026-07-29 01:25:57 +0100

    Base code

--- Searching strategies/graph.py
+++ Searching strategies/graph.py
@@ -2,6 +2,7 @@ class Graph:
     def __init__(self):
         self.nodes = {}
         self.count = 0
+        self.edges = {}
 
     def add_node(self, node:'Node'):
         if node not in self.nodes:
@@ -12,13 +13,17 @@ class Graph:
             print("Node already added")
 
     def add_edge(self, node1:'Node', node2:'Node', weight:int =0):
+        "Case of undirected graph"
         if node1 not in self.nodes:
             self.add_node(node1)
         if node2 not in self.nodes:
             self.add_node(node2)
-        self.nodes[node1].append(node2)
-        self.nodes[node2].append(node1)
-        self.weights = weight
+        if node2 not in self.nodes[node1]:
+            self.nodes[node1].append(node2)
+        if node1 not in self.nodes[node2]:
+            self.nodes[node2].append(node1)
+        self.edges[(node1, node2)] = weight
+        self.edges[(node2, node1)] = weight
 
     def contains(self, node:'Node'):
          return node in self.nodes.keys()
